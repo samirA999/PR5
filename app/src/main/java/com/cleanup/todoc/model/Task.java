1c14f11d2ae0381ae8a0ea6d813d1ed74bcdaf56
Samira <oussa41@live.fr>
2021-08-31 22:04:29 +0200

    Initial commit

--- app/src/main/java/com/cleanup/todoc/model/Task.java
+++ app/src/main/java/com/cleanup/todoc/model/Task.java
@@ -1,7 +1,8 @@
 package com.cleanup.todoc.model;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.Comparator;
 
